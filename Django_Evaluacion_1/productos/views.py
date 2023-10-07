46164e5d93a5008d75a84765294535a4c72b9420
gTaitasaur <gTaitasaur@gmail.com>
2023-10-06 21:41:02 -0300

    Initial commit

---
+++ Django_Evaluacion_1/productos/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+#Principal 
+
+def index(request):
+    return render(request, 'index/index.html')
+
+def usuario(request):
+    return render(request, 'vistas/usuario.html')
+
+def productos(request):
+    return render(request, 'vistas/productos.html')
+
+#Productos
+def electronica(request):
+    return render(request, 'vistas/electronica.html')
+
+def juguetes(request):
+    return render(request, 'vistas/juguetes.html')
+
+def ropa(request):
+    return render(request, 'vistas/ropa.html')
+
+#Descripcion
+def desc_computador(request):
+    return render(request, 'electronica_desc/desc_computador.html')
